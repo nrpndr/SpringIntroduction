039b08f63eed7e406fc4ff6f61bd7fba5b07cdea
nrpndr <nripendra.thakur87@gmail.com>
2013-11-07 17:27:03 +0530

    IDREF use case example

---
+++ src/com/my/project/spring/intro/BeanA.java
@@ -0,0 +1,29 @@
+package com.my.project.spring.intro;
+
+public class BeanA {
+	
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "BeanA [message=" + message + "]";
+	}
+	
+}
---
+++ src/com/my/project/spring/intro/ContainerForIDREF.java
@@ -0,0 +1,25 @@
+package com.my.project.spring.intro;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * The idref element is simply an error-proof way to pass the id of another bean in the container (to a
+ * <constructor-arg/> or <property/> element).
+ * using the idref tag allows the container to
+ * validate at deployment time that the referenced, named bean actually exists.
+ * @author ivy4293
+ *
+ */
+public class ContainerForIDREF {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("contextIDREF.xml");
+		BeanA beanA = (BeanA)context.getBean("beanA");
+		System.out.println(beanA);
+		BeanB beanB = (BeanB)context.getBean("beanB");
+		System.out.println(beanB);
+	}
+
+}
